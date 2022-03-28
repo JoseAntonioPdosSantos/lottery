0d9ddbf53b88b60c90b88b563d4cf9f5eb691753
José Antonio Pinto dos Santos <jose.santos@ps.otg.com.br>
2022-03-28 06:09:34 -0300

    feat: variable players created

---
+++ contracts/Loterry.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.7.0 <0.9.0;
+
+contract Lottery {
+    address public manager;
+    address[] public players;
+
+    constructor(){
+        manager = msg.sender;
+    }
+
+    function enter() public payable {
+        players.push(msg.sender);
+    }
+}
