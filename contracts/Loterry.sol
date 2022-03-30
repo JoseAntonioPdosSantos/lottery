896cba947eb9ed0d884df8f403f11a816ed2406d
José Antonio Pinto dos Santos <jose.santos@ps.otg.com.br>
2022-03-30 05:45:48 -0300

    feat: resetting contract state

--- contracts/Loterry.sol
+++ contracts/Loterry.sol
@@ -24,5 +24,6 @@ contract Lottery {
     function pickWinner() public {
         uint index = random() % players.length;
         players[index].transfer(address(this).balance);
+        players = new address payable [](0);
     }
 }
