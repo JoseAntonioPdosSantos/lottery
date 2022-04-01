51c56653eaf46a2b20e7ea86ca7623d1aaf42d4a
José Antonio Pinto dos Santos <jose.santos@ps.otg.com.br>
2022-04-01 05:36:02 -0300

    feat: create build code

---
+++ compile.js
@@ -0,0 +1,30 @@
+const path = require("path");
+const fs = require("fs");
+const solc = require("solc");
+
+const contractPath = path.resolve(__dirname, "contracts", "Lottery.sol");
+const source = fs.readFileSync(contractPath, { encoding: "utf8" });
+
+var input = {
+  language: "Solidity",
+  sources: {
+    Lottery: {
+      content: source,
+    },
+  },
+  settings: {
+    outputSelection: {
+      "*": {
+        "*": ["*"],
+      },
+    },
+  },
+};
+
+const inputJson = JSON.stringify(input);
+
+const compiledFile = solc.compile(inputJson);
+
+const contract = JSON.parse(compiledFile);
+
+module.exports = contract.contracts.Lottery.Lottery;
